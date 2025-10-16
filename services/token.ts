a55acbc6be63dc01d32eaccac98dc49d2944940a
yebology <139052533+yebology@users.noreply.github.com>
2025-10-16 22:39:25 +0700

    feat: add approve logic

--- services/token.ts
+++ services/token.ts
@@ -1,9 +1,10 @@
 import { config } from "@/components/provider/Web3Provider";
 import { readContract } from "wagmi/actions";
-import { IDRX, NUSA_TOKEN, USDT } from "./network";
+import { IDRX, NUSA_HUB, NUSA_TOKEN, USDT } from "./network";
 import { IDRX_abi } from "@/abi/IDRX_abi";
 import { USDT_abi } from "@/abi/USDT_abi";
 import { NUSA_abi } from "@/abi/NUSA_abi";
+import { decimals } from "@/utils/helper";
 
 export async function nusaBalance(address: string) {
   try {
@@ -26,6 +27,36 @@ export async function idrxBalance(address: string) {
       address: IDRX,
       functionName: "balanceOf",
       args: [address],
+    });
+    return balance;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function idrxApprove(fundAmount: number) {
+  try {
+    const convertedFundAmount = fundAmount * decimals();
+    const balance = await readContract(config, {
+      abi: IDRX_abi,
+      address: IDRX,
+      functionName: "approve",
+      args: [NUSA_HUB, convertedFundAmount],
+    });
+    return balance;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function usdtApprove(fundAmount: number) {
+  try {
+    const convertedFundAmount = fundAmount * decimals();
+    const balance = await readContract(config, {
+      abi: USDT_abi,
+      address: USDT,
+      functionName: "approve",
+      args: [NUSA_HUB, convertedFundAmount],
     });
     return balance;
   } catch (error) {
